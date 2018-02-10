4e631782b9590a2cd8fc7e6617f1e0d890870521
fwcd <fwcdmail@gmail.com>
2018-02-11 00:00:28 +0100

    Added optionals

--- src/GlucoseTest.cpp
+++ src/GlucoseTest.cpp
@@ -10,20 +10,42 @@
 #include <iostream>
 #include <string>
 
+#include "exception/NoSuchElementException.h"
 #include "math/VectorND.h"
+#include "structs/Optional.h"
+
+using namespace glucose;
 
 int main() {
-	glucose::VectorND a({1, 2, 3});
-	glucose::VectorND b({4, 5, 6});
+	std::cout << "\n==== VectorND testing ====\n\n";
 
-	std::cout << "Adding: " << (a + b).toString() << "\n";
-	std::cout << "Subtracting: " << (a - b).toString() << "\n";
-	std::cout << "Dotting: " << a.dot(b) << "\n";
-	std::cout << "Scaling: " << (a * 10).toString() << "\n";
-	std::cout << "Random Access: " << a[0] << "\n";
-	std::cout << "Normalizing: " << a.normalize().toString() << "\n";
-	std::cout << "Absolute/Length: " << a.length() << "\n";
-	std::cout << "Size: " << a.size() << "\n";
+	VectorND a({1, 2, 3});
+	VectorND b({4, 5, 6});
+
+	std::cout << "Adding:\t\t" << (a + b).toString() << "\n";
+	std::cout << "Subtracting:\t" << (a - b).toString() << "\n";
+	std::cout << "Dotting:\t" << a.dot(b) << "\n";
+	std::cout << "Scaling:\t" << (a * 10).toString() << "\n";
+	std::cout << "Random Access:\t" << a[0] << "\n";
+	std::cout << "Normalizing:\t" << a.normalize().toString() << "\n";
+	std::cout << "Abs/Length:\t" << a.length() << "\n";
+	std::cout << "Size:\t\t" << a.size() << "\n";
+
+	std::cout << "\n==== Optional testing ====\n\n";
+
+	int v = 5;
+	Optional<int> emptyInt;
+	Optional<int> filledInt(&v);
+
+	std::cout << "Empty:\t\t" << emptyInt.toString() << "\n";
+	std::cout << "Filled:\t\t" << filledInt.toString() << "\n";
+	std::cout << "Unwrapped:\t" << *filledInt << "\n";
+
+	try {
+		*emptyInt;
+	} catch (const NoSuchElementException& e) {
+		std::cout << "Whoops, almost tried to dereference an empty optional.\n";
+	}
 
 	return 0;
 }
---
+++ src/structs/Optional.h
@@ -0,0 +1,78 @@
+/*
+ * Optional.h
+ *
+ *  Created on: 10.02.2018
+ */
+
+#ifndef STRUCTS_OPTIONAL_H_
+#define STRUCTS_OPTIONAL_H_
+
+#include <functional>
+#include <sstream>
+#include <string>
+
+#include "../exception/NoSuchElementException.h"
+
+namespace glucose {
+
+/**
+ * Immutable optional type.
+ */
+template <typename T> class Optional {
+public:
+	Optional() {
+		item = NULL;
+	}
+
+	Optional(T* obj) {
+		item = obj;
+	}
+
+	virtual ~Optional() {
+		if (isPresent()) { // Only call delete if item is not null already
+			delete item;
+		}
+	}
+
+	/**
+	 * Maps the optional.
+	 */
+	template <typename R> Optional<R> map(std::function<R*(T)> mapper) {
+		return Optional<R>(mapper(*item));
+	}
+
+	bool isPresent() {
+		return item;
+	}
+
+	const T& orElse(const T& other) {
+		return isPresent() ? (*item) : other;
+	}
+
+	/**
+	 * Unwraps the optional.
+	 */
+	const T& operator *() {
+		if (isPresent()) {
+			return *item;
+		} else {
+			throw NoSuchElementException("Tried to unwrap empty optional");
+		}
+	}
+
+	std::string toString() {
+		if (isPresent()) {
+			std::ostringstream address;
+			address << static_cast<const void*>(item);
+			return "Optional pointing to " + address.str();
+		} else {
+			return "Empty optional";
+		}
+	}
+private:
+	T* item;
+};
+
+} /* namespace glucose */
+
+#endif /* STRUCTS_OPTIONAL_H_ */
